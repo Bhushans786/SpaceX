9dd7c77a828f4de3de710147a62248eb6a9a5d4e
bhushan.sharma@biz2credit.com <bhushan.sharma@biz2credit.com>
2020-11-08 20:21:23 +0530

    ssr changes

--- src/app/core/components/filter/filter.component.ts
+++ src/app/core/components/filter/filter.component.ts
@@ -39,7 +39,9 @@ export class FilterComponent implements OnInit {
         filter(event => Object.keys(event).length > 0),
         take(1))
       .subscribe((params) => {
+
         this.filterForm.patchValue(params);
+        console.log(params, this.filterForm.value);
       });
   }
 
--- src/app/shared/consts/filter-options.ts
+++ src/app/shared/consts/filter-options.ts
@@ -3,49 +3,49 @@ export const FILTEROPTIONS = [
         name: 'launch_year',
         options: [
             {
-                value: 2006
+                value: '2006'
             },
             {
-                value: 2007
+                value: '2007'
             },
             {
-                value: 2008
+                value: '2008'
             },
             {
-                value: 2009
+                value: '2009'
             },
             {
-                value: 2010
+                value: '2010'
             },
             {
-                value: 2011
+                value: '2011'
             },
             {
-                value: 2012
+                value: '2012'
             },
             {
-                value: 2013
+                value: '2013'
             },
             {
-                value: 2014
+                value: '2014'
             },
             {
-                value: 2015
+                value: '2015'
             },
             {
-                value: 2016
+                value: '2016'
             },
             {
-                value: 2017
+                value: '2017'
             },
             {
-                value: 2018
+                value: '2018'
             },
             {
-                value: 2019
+                value: '2019'
             },
             {
-                value: 2020
+                value: '2020'
             }
         ]
     },
